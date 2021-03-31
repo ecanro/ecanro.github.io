70a99ec8d7a47eb854cc106506a015e04e0f5382
Edgar Canro <edgar.canro@gmail.com>
2021-03-31 22:55:26 +0100

    Delete post.py

--- post.py
+++
@@ -1,8 +0,0 @@
-class Post:
-    def __init__(self, post_id, date, title, posted, subtitle, body):
-        self.id = post_id
-        self.date = date
-        self.title = title
-        self.posted = posted
-        self.subtitle = subtitle
-        self.body = body
